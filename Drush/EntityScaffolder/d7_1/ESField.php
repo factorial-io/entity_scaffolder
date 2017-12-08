ce2b66ac13d5135fdfd9888146a8da57aef41e62
D34dMan <shibin@factorial.io>
2017-12-08 13:10:17 +0530

    When trying to use invalid field machine name, provide a warning message

--- Drush/EntityScaffolder/d7_1/ESField.php
+++ Drush/EntityScaffolder/d7_1/ESField.php
@@ -3,6 +3,7 @@
 namespace Drush\EntityScaffolder\d7_1;
 
 use Drush\EntityScaffolder\Utils;
+use Drush\EntityScaffolder\Logger;
 
 class ESField extends ESBase {
 
@@ -24,7 +25,12 @@ class ESField extends ESBase {
    * Helper function to generate machine name for fields.
    */
   public function getFieldName($config, $field_key) {
+    $original_field_key = $field_key;
     $field_key = strtolower($field_key);
+    $field_key = str_replace(' ', '_', $field_key);
+    if ($original_field_key != $field_key) {
+      Logger::log('Field name "' . $original_field_key . '" is invalid, using "' . $field_key . '" instead', 'warning');
+    }
     return $config['field_prefix'] . '_' . $field_key;
   }
 
