f886fc17ec010e48ee676130da0d99f7a5c643dd
D34dMan <shibin@factorial.io>
2018-10-25 01:03:11 +0200

    Bump version to 7.1.4

--- Drush/EntityScaffolder/d7_1/Scaffolder.php
+++ Drush/EntityScaffolder/d7_1/Scaffolder.php
@@ -9,7 +9,7 @@ use Drush\EntityScaffolder\Logger;
 
 class Scaffolder extends ScaffolderBase {
   // @see http://php.net/version_compare.
-  const VERSION = '7.1.3';
+  const VERSION = '7.1.4';
 
   const DEFAULT_TEMPLATE_DIR = __DIR__ . '/templates';
   const TEMPLATE_NAMESPACE = 'd7_1';
