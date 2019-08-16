bc129f1240c932f0dd09faaabd16bb49feb29604
D34dMan <shibin@factorial.io>
2019-08-16 16:28:12 +0200

    Remove debug message

--- Drush/EntityScaffolder/d7_1/ESConfig.php
+++ Drush/EntityScaffolder/d7_1/ESConfig.php
@@ -41,7 +41,6 @@ class ESConfig extends ESBase implements ESBaseInterface {
       $key = 0;
       $template = '/config/es_helper.potx_exportables.inc';
       $code = $this->scaffolder->render($template, $info['translation']);
-      Logger::debug($info['translation']['po_dir'], '$info');
       $this->scaffolder->setCode($module, $filename, $block, $key, $code);
     }
   }
